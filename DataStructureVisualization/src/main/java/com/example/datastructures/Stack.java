754dbc42b0f79c0c50ce24d0b45aa8533d349ac8
Aprameya Kannan <101409453+aprameyak@users.noreply.github.com>
2025-04-07 14:53:41 -0400

    implemented stack

--- DataStructureVisualization/src/main/java/com/example/datastructures/Stack.java
+++ DataStructureVisualization/src/main/java/com/example/datastructures/Stack.java
@@ -1,5 +1,44 @@
 package com.example.datastructures;
 
-public class Stack {
+public class Stack<T> {
+    T[] stack;
+    int size;
+    int capacity;
 
+    public Stack(int capacity) {
+        this.size = size;
+        stack = (T[]) new Object[capacity];
+        this.size = 0;
+    }   
+    public void push(T t) {
+        if(size == capacity) {
+            resize()
+        }
+        stack[size++] = t;
+    }
+    public T pop() {
+        if(isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        T t = stack[--size];
+        stack[size] = null;
+        return t;
+    }
+    public T peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return stack[size - 1]; 
+    }
+    public boolean isEmpty() {
+        return size == 0;
+    }   
+    private void resize() {
+        T[] newStack = (T[]) new Object[2 * capacity]; 
+        for (int i = 0; i < size; i++) {
+            newStack[i] = stack[i]; 
+        }
+        stack = newStack; 
+        capacity = 2 * capacity; 
+    }
 }
