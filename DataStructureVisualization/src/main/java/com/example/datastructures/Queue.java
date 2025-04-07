f6060f457947b82b24500588f016150ad6bcc2b7
Aprameya Kannan <101409453+aprameyak@users.noreply.github.com>
2025-04-07 14:57:12 -0400

    implemented queue

--- DataStructureVisualization/src/main/java/com/example/datastructures/Queue.java
+++ DataStructureVisualization/src/main/java/com/example/datastructures/Queue.java
@@ -1,5 +1,47 @@
 package com.example.datastructures;
 
-public class Queue {
+public class Queue<T> {
+    T[] queue;
+    int size;
+    int capacity;
 
+    public Queue(int capacity) {
+        this.size = size;
+        queue = (T[]) new Object[capacity];
+        this.size = 0;
+    }   
+    public void push(T t) {
+        if(size == capacity) {
+            resize()
+        }
+        queue[size++] = t;
+    }
+    public T pop() {
+        if(isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        T t = queue[0];
+        size--;
+        for (int i = 0; i < size; i++) {
+            queue[i] = queue[i + 1]; 
+        }
+        return t;
+    }
+    public T peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        return queue[0]; 
+    }
+    public boolean isEmpty() {
+        return size == 0;
+    }   
+    private void resize() {
+        T[] newQueue = (T[]) new Object[2 * capacity]; 
+        for (int i = 0; i < size; i++) {
+            newQueue[i] = queue[i]; 
+        }
+        queue = newQueue; 
+        capacity = 2 * capacity; 
+    }
 }
